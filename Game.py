015d9c1c880f6069e26386d9d979b8e7102ee262
jazielrivera <jazie.rivera@upr.edu>
2025-09-05 12:19:27 -0400

    Task 1 completed

--- Game.py
+++ Game.py
@@ -77,7 +77,7 @@ bg_color = pygame.Color('grey12')
 ball = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 30, 30)  # Ball (centered)
 # TODO Task 1 Make the paddle bigger
 player_height = 15
-player_width = 160
+player_width = 140
 player = pygame.Rect(screen_width/2 - 45, screen_height - 20, player_width, player_height)  # Player paddle
 
 # Game Variables
